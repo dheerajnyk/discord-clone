9683707fa57c5f42fd2659ef4ba65ac32c61e275
dheerajnyk <dheerajnayak1995@gmail.com>
2021-09-06 01:08:47 +0530

    Created mongoschema for the database and api's

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,7 +1,12 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    // your firebase config
+    apiKey: "AIzaSyDPs-socGOqklEm6jGNUqhxo6pPda1IjCM",
+    authDomain: "discord-clone-16ab0.firebaseapp.com",
+    projectId: "discord-clone-16ab0",
+    storageBucket: "discord-clone-16ab0.appspot.com",
+    messagingSenderId: "951874082401",
+    appId: "1:951874082401:web:67b04683bc3ab9b67e3c20"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
--- server.js
+++ server.js
@@ -1,14 +1,58 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongo from './api.js';
+
+// env config
+dotenv.config();
+const port = process.env.PORT || 5002
+const uri = process.env.MONGOURI
+
+//db config
+mongoose.connect(uri);
 
 var app = express()
-const port = process.env.PORT || 5002
 
+//middlewear
+app.use(express.json())
+app.use(cors())
 
+//api's
 app.get('/', (req,res)=>{
     res.send("lol")
 } )
 
 app.listen(port,()=>{
     console.log(`listening on port : ${port}`)
+})
+
+app.post('/new/channel',(req,res)=>{
+    var data = req.body
+    mongo.create(data,(err,data)=>{
+        if(err)res.status(500).send("Failed to create")
+        else{
+            res.status(201).send("created new channel")
+        }
+    })
+})
+
+app.get('/get/channelList',(req,res)=>{
+    var data = req.body
+    mongo.find(data,(err,data)=>{
+        if(err)res.status(500).send("Failed to create")
+        else{
+            var channel = data.map(e => ({
+                'id' : e._id,
+                'name' : e.channelName
+            }))
+            res.status(200).send(channel)
+        }
+    })
+})
+
+app.post('new/message',(req,res)=>{
+    const id = req.query.id
+    const newmsg = req.body
+    mongo.updateOne({_id:id},{$push:{conversation : req.body}})
 })
